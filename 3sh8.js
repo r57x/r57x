d0e0a349b42622ebbc23c036ad4258e03f142599
k4i <44825783+r57x@users.noreply.github.com>
2022-05-22 07:12:06 +0300

    Add files via upload

---
+++ 3sh8.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$("#login-btn-holder").append('<a href="/RED.html" class="login-btn-holder__btn btn btn-primary rafat1 ">الاشتركات</a>'),
+$("#login-btn-holder").append('<a href="/RULS.html" class="login-btn-holder__btn btn btn-primary rafat1 ">القوانين</a>'),
+$("#login-btn-holder").append('<a href="/RIW.html" class="login-btn-holder__btn btn btn-primary rafat1 ">اتصل بنا</a>'),
+$("#login-btn-holder").append('<a href="https://app.bnotah.net/" class="login-btn-holder__btn btn btn-primary rafat1 ">حمل التطبيق</a>');
+var n=["prorafat","toggleClass","click","label#login-form__hidden-label"];$(n[3])[n[2]](function(){$(this)[n[1]](n[0])})});var _0x56bb=['<li id="chat__footer__menu__notifications_item" class="border border-color btn-rok chat__footer__menu__item"><span class="fa fa-bell"></span></li>',"append","#chat__footer","click","div#chat__body__settings_pane__container__button--notifications","li#chat__footer__menu__notifications_item","ready","button#guest-form__submit, button#login-form__submit"];$(_0x56bb[7])[_0x56bb[3]](function(){jQuery(document)[_0x56bb[6]](function(){setTimeout(function(){$(_0x56bb[2])[_0x56bb[1]](_0x56bb[0]),$(_0x56bb[5])[_0x56bb[3]](function(){$(_0x56bb[4])[_0x56bb[3]]()})},1e3)})});
+$("button#guest-form__submit, button#login-form__submit").click(function(){
+jQuery(document).ready(function() {
+    setTimeout(function() {
+
+$(".chat_pane__container__banner").append("<div class='rafatp1'><span class='name'>مبدع الحائط </span></div>");
+
+$( ".rafatp1" ).click(function() {
+  $( "li#chat__footer__menu__wall_creator_item" ).click();
+});
+
+    }, 1500);
+});
+});
